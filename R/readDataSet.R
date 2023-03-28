14c2d698ab7a76cf4cbecede4d545659f92253ea
Kucharssim <kucharssim@gmail.com>
2023-03-28 12:50:52 +0200

    readHeaderFromR and column specification

--- R/readDataSet.R
+++ R/readDataSet.R
@@ -1,10 +1,21 @@
+#' @export
 .setDataSet <- function(dataset) {
   .internal[["dataset"]] <- dataset
 }
 
 .readDataSetToEndFromR <- function(columns=NULL, columns.as.numeric=NULL, columns.as.ordinal=NULL, columns.as.factor=NULL, all.columns=FALSE, ...) {
   dataset <- .internal[["dataset"]]
-  
+  dataset <- .dataSetSubsetColumns(dataset, columns=columns, columns.as.numeric=columns.as.numeric, columns.as.ordinal=columns.as.ordinal, columns.as.factor=columns.as.factor, all.columns=all.columns, ...)
+  return(dataset)
+}
+
+.readDataSetHeaderFromR <- function(columns=NULL, columns.as.numeric=NULL, columns.as.ordinal=NULL, columns.as.factor=NULL, all.columns=FALSE, ...) {
+  dataset <- .readDataSetToEndFromR(columns=columns, columns.as.numeric=columns.as.numeric, columns.as.ordinal=columns.as.ordinal, columns.as.factor=columns.as.factor, all.columns=all.columns, ...)
+  dataset <- dataset[NULL, ]
+  return(dataset)
+}
+
+.dataSetSubsetColumns <- function(dataset, columns=NULL, columns.as.numeric=NULL, columns.as.ordinal=NULL, columns.as.factor=NULL, all.columns=FALSE, ...) {
   if(!all.columns) {
     dataset <- dataset[, unique(c(columns, columns.as.numeric, columns.as.ordinal, columns.as.factor)), drop = FALSE]
   }
@@ -26,4 +37,19 @@
   as.data.frame(
     lapply(columns, type)
   )
+}
+
+#' @export
+.dataSetColumnSpecification <- function() {
+  dataset <- .readDataSetHeaderFromR(all.columns = TRUE)
+  specification <- list()
+  for(i in seq_len(ncol(dataset))) {
+    cl <- class(dataset[[i]])[1]
+    specification[[i]] <- list(
+      name = colnames(dataset)[[i]],
+      type = cl,
+      labels = if(cl %in% c("factor", "ordered")) levels(dataset[[i]]) else NULL
+    )
+  }
+  return(specification)
 }
